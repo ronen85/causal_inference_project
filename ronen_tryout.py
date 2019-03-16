80824b6f7aa9992f7bf0fc08299d5d5973294542
Ronen Nizri <ronen.nizri@gmail.com>
2019-03-16 18:50:54 +0300

    add ronen's work (ATE for country)

--- ronen_tryout.py
+++ ronen_tryout.py
@@ -56,76 +56,124 @@ def get_ate(df, country_name, year):
     ATE = Y1 - Y0
     return ATE
 
+def remove_nan_from_df(dataframe, inplace = True):
+    # removes nan values
+    dataframe = dataframe.loc[(~np.isnan(dataframe.suicides_no)) & (~np.isnan(dataframe.population))]
+    return dataframe
+
+def check_num_of_records(dataframe, country_name, year):
+    num_records = dataframe.loc[(dataframe.country == country_name) & (dataframe.year == year)].index.size
+    return num_records == 12
+
 if __name__ == '__main__':
     make_new_df = True
+    compute_ate_for_year = False
 
     if make_new_df:
         df = load_dataframe()
         # raw_df = pd.read_csv('./data/WHO.csv', header=0)
 
     """compute ate for the wc in year X"""
-    wc_year = 2010
-    # take only relevant years
-    df_ate = df.loc[df.year.isin([wc_year - 1, wc_year, wc_year + 1])]
-    # remove nan values
-    df_ate = df_ate.loc[(~np.isnan(df_ate.suicides_no))]
-    df_ate = df_ate.loc[(~np.isnan(df_ate.population))]
-    # remove countries with missing data
-    cs = df_ate.country.unique()
-    l = list(range(wc_year - 1, wc_year + 2))
-    for c in cs:
-        if np.sum(tuple((df_ate.country == c) & (df_ate.year.isin(l)))) != 36:
-            print('Missing data in %s in years around %s'%(c,wc_year))
-            df_ate.drop(df_ate.loc[df.country == c].index, inplace=True)
-    # remove small countries
-    min_population = 5*10**6
-    cs = list(df_ate.country.unique())
-    country_size_dict = {}
-    for c in cs:
-        country_size_dict[c] = np.sum(df_ate.loc[(df_ate.country == c) &
-                                         (df_ate.year.unique()[0] == df_ate.year)].population)
-        # country_size = np.sum(df_ate.loc[(df_ate.country == c) &
-        #                                  (df_ate.year.unique()[0] == df_ate.year)].population)
-        if country_size_dict[c] < min_population:
-            print('%s is too small, we dropped it'%(c,))
-            df_ate.drop(df_ate.loc[df.country == c].index, inplace=True)
-            df_ate.drop(df_ate.loc[(df_ate.country == c)].index, inplace=True)
-    # remove zeros
-    cs = list(df_ate.country.unique())
-    ys = list(df_ate.year.unique())
-    for c in cs:
-        for y in ys:
-            sum_of_suicide = np.sum(df_ate.loc[(df_ate.country == c) & (df_ate.year == y)].suicides_no)
-            if sum_of_suicide == 0:
-                # print('in %s in the year %d the number of suicides is zero'%(c,y))
-                df_ate.drop(df_ate.loc[(df_ate.country == c) & (df_ate.year == y)].index, inplace=True)
-    # collect data
-    ate_dict = {}
-    cs = list(df_ate.country.unique())
-    for c in cs:
-        ate_dict[c] = (get_ate(df_ate,c,wc_year), country_size_dict[c])
-    # compute ate
-    naive_ate = np.average(tuple(a[0] for a in tuple(ate_dict.values()))) # average ate without considering population
-    average_ate = np.sum(tuple(a[0]*a[1] for a in tuple(ate_dict.values())))/\
-                  np.sum(tuple(a[1] for a in tuple(ate_dict.values()))) # average ate considering the population
-    # compute ate for participants only
-    cs = [k for k in ate_dict.keys() if k in WC_participants_by_year[wc_year]]
-    parts_ate_dict = {}
-    for c in cs:
-        parts_ate_dict[c] = ate_dict[c]
-    parts_naive_ate = np.average(tuple(a[0] for a in tuple(parts_ate_dict.values())))  # average ate without considering population
-    parts_average_ate = np.sum(tuple(a[0] * a[1] for a in tuple(parts_ate_dict.values()))) / \
-                  np.sum(tuple(a[1] for a in tuple(parts_ate_dict.values())))  # average ate considering the population
+    if compute_ate_for_year:
+        wc_year = 2010
+        # take only relevant years
+        df_ate = df.loc[df.year.isin([wc_year - 1, wc_year, wc_year + 1])]
+        # remove nan values
+        df_ate = df_ate.loc[(~np.isnan(df_ate.suicides_no))]
+        df_ate = df_ate.loc[(~np.isnan(df_ate.population))]
+        # remove countries with missing data
+        cs = df_ate.country.unique()
+        l = list(range(wc_year - 1, wc_year + 2))
+        for c in cs:
+            if np.sum(tuple((df_ate.country == c) & (df_ate.year.isin(l)))) != 36:
+                print('Missing data in %s in years around %s'%(c,wc_year))
+                df_ate.drop(df_ate.loc[df.country == c].index, inplace=True)
+        # remove small countries
+        min_population = 5*10**6
+        cs = list(df_ate.country.unique())
+        country_size_dict = {}
+        for c in cs:
+            country_size_dict[c] = np.sum(df_ate.loc[(df_ate.country == c) &
+                                             (df_ate.year.unique()[0] == df_ate.year)].population)
+            # country_size = np.sum(df_ate.loc[(df_ate.country == c) &
+            #                                  (df_ate.year.unique()[0] == df_ate.year)].population)
+            if country_size_dict[c] < min_population:
+                print('%s is too small, we dropped it'%(c,))
+                df_ate.drop(df_ate.loc[df.country == c].index, inplace=True)
+                df_ate.drop(df_ate.loc[(df_ate.country == c)].index, inplace=True)
+        # remove zeros
+        cs = list(df_ate.country.unique())
+        ys = list(df_ate.year.unique())
+        for c in cs:
+            for y in ys:
+                sum_of_suicide = np.sum(df_ate.loc[(df_ate.country == c) & (df_ate.year == y)].suicides_no)
+                if sum_of_suicide == 0:
+                    # print('in %s in the year %d the number of suicides is zero'%(c,y))
+                    df_ate.drop(df_ate.loc[(df_ate.country == c) & (df_ate.year == y)].index, inplace=True)
+        # collect data
+        ate_dict = {}
+        cs = list(df_ate.country.unique())
+        for c in cs:
+            ate_dict[c] = (get_ate(df_ate,c,wc_year), country_size_dict[c])
+        # compute ate
+        naive_ate = np.average(tuple(a[0] for a in tuple(ate_dict.values()))) # average ate without considering population
+        average_ate = np.sum(tuple(a[0]*a[1] for a in tuple(ate_dict.values())))/\
+                      np.sum(tuple(a[1] for a in tuple(ate_dict.values()))) # average ate considering the population
+        # compute ate for participants only
+        cs = [k for k in ate_dict.keys() if k in WC_participants_by_year[wc_year]]
+        parts_ate_dict = {}
+        for c in cs:
+            parts_ate_dict[c] = ate_dict[c]
+        parts_naive_ate = np.average(tuple(a[0] for a in tuple(parts_ate_dict.values())))  # average ate without considering population
+        parts_average_ate = np.sum(tuple(a[0] * a[1] for a in tuple(parts_ate_dict.values()))) / \
+                      np.sum(tuple(a[1] for a in tuple(parts_ate_dict.values())))  # average ate considering the population
 
+        # plot it
+        figure(0)
+        plt.title("Change in Suicide Rates in %s"%wc_year)
+        plt.ylabel('ATE = Y1 - Y0')
+        plt.xlabel('Countries')
+        barlist = plt.bar(range(len(ate_dict)), list(a[0] for a in ate_dict.values()), align='center')
+        for i in range(len(ate_dict)):
+            if list(ate_dict.keys())[i] in WC_participants_by_year[wc_year]:
+                barlist[i].set_color('r')
+        plt.xticks(range(len(ate_dict)), list(ate_dict.keys()), rotation='vertical')
+        plt.show()
+
+    """compute ate for one country"""
+    country = "France"
+    df_ate = df.loc[df.country == country]
+    # remove nan values
+    df_ate = remove_nan_from_df(df_ate)
+    # remove wc years with missing data
+    ys = []
+    for y in WC_years:
+        check = [check_num_of_records(df_ate,country,a) for a in range(y-1,y+2)]
+        if not(False in check):
+            ys.append(y)
+    # compute ate
+    ate_dict = {}
+    for y in ys:
+        ate_dict[y] = get_ate(df_ate,country,y)
     # plot it
-    plt.title("Change in Suicide Rates in 2010")
+    plt.figure(1)
+    plt.title("Change in Suicide Rates in %s" % country)
     plt.ylabel('ATE = Y1 - Y0')
-    plt.xlabel('Carrier')
-    barlist = plt.bar(range(len(ate_dict)), list(a[0] for a in ate_dict.values()), align='center')
+    plt.xlabel('Years')
+    barlist = plt.bar(range(len(ate_dict)), ate_dict.values(), align='center')
     for i in range(len(ate_dict)):
-        if list(ate_dict.keys())[i] in WC_participants_by_year[wc_year]:
+        if country in WC_participants_by_year[list(ate_dict.keys())[i]]:
             barlist[i].set_color('r')
     plt.xticks(range(len(ate_dict)), list(ate_dict.keys()), rotation='vertical')
     plt.show()
-    print('test')
 
+
+
+
+
+
+    print('tez')
+    # df_ate = df_ate.loc[(~np.isnan(df_ate.suicides_no))]
+    # df_ate = df_ate.loc[(~np.isnan(df_ate.population))]
+    # print('test')
+    #
